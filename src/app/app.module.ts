32178e1583cb4ffb8918a583d9da8b2b06f9bd19
Gary Sui <guanghua.sui@greenskycredit.com>
2017-12-01 17:28:18 -0500

    refactoring header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { RootComponent } from './components/root/root.component';
 import { HomeComponent } from './components/home/home.component';
 import { Bs4Component } from './components/bs4/bs4.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     RootComponent,
     HomeComponent,
-    Bs4Component
+    Bs4Component,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
