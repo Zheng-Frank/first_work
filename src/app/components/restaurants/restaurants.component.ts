abdbd24050134d91f579aa9564b039485003d335
gary sui <garysui@gmail.com>
2018-03-24 21:30:23 -0400

    added crawl SL function

--- src/app/components/restaurants/restaurants.component.ts
+++ src/app/components/restaurants/restaurants.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { ApiService } from "../../services/api.service";
 import { environment } from "../../../environments/environment";
 import { GlobalService } from "../../services/global.service";
+import { AlertType } from "../../classes/alert-type";
+import { saveAs } from "file-saver";
 
 @Component({
   selector: "app-restaurants",
@@ -11,13 +13,35 @@ import { GlobalService } from "../../services/global.service";
 export class RestaurantsComponent implements OnInit {
   constructor(private _api: ApiService, private _global: GlobalService) {}
 
+  showCrawl = false;
+  crawling = false;
+  crawUrl;
+
   ngOnInit() {}
-
-  sync() {
-    // pull existing production restaurants (using name and place_id???)
-    // add to service provider, if not in
-    // add to data collected 
+  toggleCrawl() {
+    this.showCrawl = !this.showCrawl;
   }
-
+  getRestaurantData() {
+    this.showCrawl = true;
+    this.crawling = true;
+    this._api
+      .get(environment.adminApiUrl + "utils/crawl-restaurant", {
+        url: this.crawUrl
+      })
+      .subscribe(
+        result => {
+          this.crawling = false;
+          saveAs(
+            new Blob([JSON.stringify(result)], { type: "text" }),
+            "data.json"
+          );
+        },
+        error => {
+          this.crawling = true;
+          this._global.publishAlert(AlertType.Danger, "Failed to crawl");
+          console.log(error);
+        }
+      );
+  }
   createNew() {}
 }
