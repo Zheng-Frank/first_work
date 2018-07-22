7835d83de1c2b07af5e66cc92158e3712f6b31a9
Gary Sui <garysui@gmail.com>
2018-07-22 15:52:28 -0400

    use biz.qmenu instead of api.myqmenu

--- src/app/components/invoices/invoice-details/invoice-details.component.ts
+++ src/app/components/invoices/invoice-details/invoice-details.component.ts
@@ -253,8 +253,8 @@ export class InvoiceDetailsComponent implements OnInit, OnDestroy {
   sendInvoice(channel: Channel) {
     this.apiRequesting = channel.type;
     // we need to get shorten URL, mainly for SMS.
-    // const url = environment.bizUrl + 'index.html#/invoice/' + (this.invoice.id || this.invoice['_id']);
-    const url = environment.legacyApiUrl + 'utilities/invoice/' + (this.invoice.id || this.invoice['_id']);
+    const url = environment.bizUrl + 'index.html#/invoice/' + (this.invoice.id || this.invoice['_id']);
+    // const url = environment.legacyApiUrl + 'utilities/invoice/' + (this.invoice.id || this.invoice['_id']);
 
     this._api.get(environment.legacyApiUrl + 'utilities/getShortUrl', { longUrl: url }).pipe(mergeMap(shortUrl => {
       let message = 'QMENU INVOICE:';
