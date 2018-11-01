df39424e72b16fc80495d74487fea21c06d39425
Gary Sui <garysui@gmail.com>
2018-11-01 11:34:57 -0400

    added build qa

---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  env: "qa",
+  adminApiUrl: 'https://swozix2epl.execute-api.us-east-2.amazonaws.com/dev/',
+  qmenuApiUrl: 'https://67dqylz39g.execute-api.us-east-2.amazonaws.com/dev/',
+  legacyApiUrl: "https://quez.herokuapp.com/",
+  autoGmbUrl: "http://localhost:3000/",
+  socketUrl: "https://quez.herokuapp.com:443/",
+  bizUrl: 'https://restaurant-quez.herokuapp.com/',
+  customerUrl: 'https://customer-app.herokuapp.com/',
+  thumnailUrl: 'https://s3.amazonaws.com/chopstresized/128_menuImage/',
+  normalResUrl: 'https://s3.amazonaws.com/chopstresized/768_menuImage/',
+  stripePublishableKey: 'pk_test_PbXsKYfVeKPV4wlQP4wpQYfg',
+};
