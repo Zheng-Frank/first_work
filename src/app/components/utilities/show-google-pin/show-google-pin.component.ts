6f4c3b8b8d9e1f22f3c9495278b96e8ebab5d45d
unknown <mega5329@yahoo.com>
2019-01-18 15:11:21 -0500

    add task info

--- src/app/components/utilities/show-google-pin/show-google-pin.component.ts
+++ src/app/components/utilities/show-google-pin/show-google-pin.component.ts
@@ -14,7 +14,7 @@ export class ShowGooglePINComponent implements OnInit {
     @Input() restaurant;
     @Input() bizList;
     @Input() tasks;
-    
+
     restaurantList = [];
     rows = [];
     messageTo;
@@ -29,7 +29,6 @@ export class ShowGooglePINComponent implements OnInit {
     }
 
     async populate() {
-
         const daysAgo = function (days) {
             const d = new Date();
             d.setHours(0, 0, 0, 0);
@@ -37,13 +36,13 @@ export class ShowGooglePINComponent implements OnInit {
             return d;
         };
 
+        //Retrieve Google PIN from gmb task which having saved code
         const codeList = await this._api.get(environment.adminApiUrl + "generic", {
             resource: "task",
             query: {
                 "transfer.code": {
                     "$exists": true
                 }
-
             },
             projection: {
                 'transfer.code': 1
@@ -56,60 +55,156 @@ export class ShowGooglePINComponent implements OnInit {
 
         let codes = codeList.map(each => each.transfer.code);
 
-        for (let i = 0; i < this.restaurantList.length; i++) {
-            let restaurant = this.restaurantList[i];
-            (restaurant.logs || []).map(eachLog=>{
-                if(eachLog.type==='google-pin'){
-                    this.rows.push({
-                        restaurant: [restaurant],
-                        from:'Call Log',
-                        text: eachLog.response,
-                        done: codes.some(code => code == eachLog.response)
-                    })
-                    //console.log('this.rows', this.rows);
-                }
-            })
-        }
-        
-        const results = await this._api.get(environment.qmenuApiUrl + 'generic', {
-            
+        //Retrieve Google PIn which got from SMS reply
+        const googlePinEvents = await this._api.get(environment.qmenuApiUrl + 'generic', {
             resource: 'event',
             query: {
                 "name": "google-pin"
             },
             projection: {
                 name: 1,
-                params: 1
+                params: 1,
+                createdAt: 1
             },
             limit: 10000
         }).toPromise();
-        this.rows = this.rows.concat(results.map(each => {
+
+        //Populatge Google PIN from Call Log
+        for (let i = 0; i < this.restaurantList.length; i++) {
+            let restaurant = this.restaurantList[i];
+            (restaurant.logs || []).map(eachLog => {
+                if (eachLog.type === 'google-pin') {
+                    this.rows.push({
+                        gmbBiz: this.getGmbBizFromRestaurant(restaurant),
+                        from: 'Call Log',
+                        text: eachLog.response,
+                        time: eachLog.time,
+                        done: codes.some(code => code == eachLog.response)
+                    })
+                    //console.log('this.rows', this.rows);
+                }
+            })
+        }
+        //Populatge Google PIN from SMS reply
+        this.rows = this.rows.concat(googlePinEvents.map(each => {
             return {
-                restaurant: this.getRestaurant(each.params.body.From),
+                gmbBiz: this.getGmbBizFromPhone(each.params.body.From),
                 from: each.params.body.From.length == 11 ? each.params.body.From.toString().substring(1) : each.params.body.From,
                 text: each.params.body.Text,
+                time: each.createdAt,
                 done: codes.some(code => code == each.params.body.Text)
             }
         }));
-        //console.log('final result', this.rows);
+        //console.log('rows', JSON.stringify(this.rows));
     }
 
-    getRestaurant(phoneNumber) {
-        let result = []
-        if (phoneNumber.toString().length == 11) {
-            phoneNumber = phoneNumber.toString().substring(1);
+    getTime(time) {
+        return new Date(time);
+    }
+
+    getGmbBizFromRestaurant(restaurant) {
+
+        return [{
+            restaurant: restaurant,
+            tasks: this.tasks.filter(t => t.gmbBiz && t.gmbBiz.qmenuId === restaurant['_id'])
         }
 
+        ]
+
+
+
+        // let mathingBiz=[];
+
+        //     for (let k = 0; k < this.tasks.length; k++) {
+        //             if (this.tasks[k].gmbBiz && this.tasks[k].gmbBiz.qmenuId === restaurant['_id']) {
+        //                 mathingBiz.push({
+        //                     restaurant: restaurant,
+        //                     task: this.tasks[k]
+        //                 })
+
+        //             }
+
+        //     }
+
+
+        //     //Find matching 
+        //     console.log('mathingBiz', mathingBiz);
+        //     return mathingBiz;
+
+
+
+    }
+    getGmbBizFromPhone(input) {
+        if (input.toString().length == 11) {
+            input = input.toString().substring(1);
+        }
+        let matchingRTs = [];
+        //this.restaurantList.filter(eachRt => eachRt.channels && eachRt.channels.some(p => p.type === 'Phone' && p.value === input));
+
         for (let i = 0; i < this.restaurantList.length; i++) {
-            const restaurant = this.restaurantList[i];
-            if ((restaurant.phones || []).some(phone => (phone.phoneNumber || '').indexOf(phoneNumber) === 0)) {
-                result.push(restaurant);
+            let restaurant = this.restaurantList[i];
+            if ((restaurant.channels || []).some(c => (c.type === 'Phone' || c.type === 'SMS') && c.value === input)) {
+                matchingRTs.push(restaurant)
+
             }
         }
+
+        // one phone number, can have multiple matching restaurants, and each restaurant, will have multiple tasks
+        let result = matchingRTs.map(each => {
+            let matchingTasks = this.tasks.filter(t => t.gmbBiz && t.gmbBiz.qmenuId === each['_id']);
+            return {
+                restaurant: each,
+                tasks: matchingTasks
+            }
+        })
+
         //console.log('result', result);
         return result;
 
+
+
+
     }
+
+    // getGmbBizFromPhone(phoneNumber) {
+    //     let mathingBiz=[];
+    //     let matchingRTs=[];
+    //     if (phoneNumber.toString().length == 11) {
+    //         phoneNumber = phoneNumber.toString().substring(1);
+    //     }
+
+    //     //Find matching restaurant with the SMS number
+    //     for (let i = 0; i < this.restaurantList.length; i++) {
+    //         const restaurant = this.restaurantList[i];
+    //         if ((restaurant.phones || []).some(phone => (phone.phoneNumber || '').indexOf(phoneNumber) === 0)) {
+    //         matchingRTs.push(restaurant)
+
+    //         }
+    //     }
+
+    //     for (let i = 0; i < matchingRTs.length; i++) {
+    //         for (let k = 0; k < this.tasks.length; k++) {
+    //                 if (this.tasks[k].gmbBiz && this.tasks[k].gmbBiz.qmenuId === matchingRTs[i]['_id']) {
+    //                     mathingBiz.push({
+    //                         restaurant: matchingRTs[i],
+    //                         task: this.tasks[k]
+    //                     })
+
+    //                 }
+
+    //         }
+
+
+    //         //Find matching 
+    //         console.log('mathingBiz', mathingBiz);
+    //         return mathingBiz;
+
+
+    //     }
+
+
+    // }
+
 
 
 }
