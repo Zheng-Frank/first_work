b1b62d7c1e0926f1ec939823ee9eb7bde55f4ba6
unknown <mega5329@yahoo.com>
2019-02-23 19:01:45 -0500

    bug fix for assigneeList in log editor

--- src/app/components/logs/log-editor/log-editor.component.ts
+++ src/app/components/logs/log-editor/log-editor.component.ts
@@ -78,28 +78,6 @@ export class LogEditorComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (this._global.user.roles.some(r => r === 'ADMIN')) {
-      // grab all users and make an assignee list!
-      this._api
-        .get(environment.adminApiUrl + "generic", {
-          resource: "user",
-          limit: 1000
-        })
-        .subscribe(
-        result => {
-          this.assigneeList = result.map(u => new User(u)).sort((a, b) => a.username.toLowerCase().localeCompare(b.username.toLowerCase()));
-          console.log('assigneeList', this.assigneeList);
-        },
-        error => {
-          this._global.publishAlert(
-            AlertType.Danger,
-            "Error pulling users from API"
-          );
-        }
-        );
-    }
-
-
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -119,7 +97,8 @@ export class LogEditorComponent implements OnInit {
     }
   }
 
-  changeHasTask() {
+  async changeHasTask() {
+    this.assigneeList = await this._global.getCachedUserList();
   }
 
   reset() {
