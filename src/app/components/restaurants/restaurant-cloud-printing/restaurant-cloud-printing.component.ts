ac097800df621610cf85d55b78f2e853b5ecc4cc
gary sui <garysui@gmail.com>
2019-03-29 21:52:39 -0400

    updated to allow editing customized rendering styles

--- src/app/components/restaurants/restaurant-cloud-printing/restaurant-cloud-printing.component.ts
+++ src/app/components/restaurants/restaurant-cloud-printing/restaurant-cloud-printing.component.ts
@@ -11,7 +11,29 @@ import { environment } from 'src/environments/environment';
 export class RestaurantCloudPrintingComponent implements OnInit {
   @Input() restaurant: Restaurant;
 
+  editing = false;
+  customizedRenderingStyles;
+
   constructor(private _api: ApiService) { }
+
+  toggleEditing() {
+    this.editing = !this.editing;
+    this.customizedRenderingStyles = this.restaurant.customizedRenderingStyles;
+  }
+
+  async update() {
+
+    const customizedRenderingStyles = (this.customizedRenderingStyles || '').trim();
+    if (this.restaurant.customizedRenderingStyles !== customizedRenderingStyles) {
+      await this._api.patch(environment.qmenuApiUrl + 'generic?resource=restaurant', [{
+        old: { _id: this.restaurant._id, customizedRenderingStyles: this.restaurant.customizedRenderingStyles },
+        new: { _id: this.restaurant._id, customizedRenderingStyles: customizedRenderingStyles }
+      }]).toPromise();
+      this.restaurant.customizedRenderingStyles = customizedRenderingStyles;
+    }
+    this.editing = false;
+  }
+
   async ngOnInit() {
     console.log(this.restaurant['printClients'])
     if (!this.restaurant['printClients']) {
