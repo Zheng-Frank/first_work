24e96e809915cb95f8516f8233225291c8574d21
garysui <garysui@gmail.com>
2019-09-04 19:24:38 -0400

    added row number to cycle details

--- src/app/components/invoices/cycle-details/cycle-details.component.ts
+++ src/app/components/invoices/cycle-details/cycle-details.component.ts
@@ -88,6 +88,7 @@ export class CycleDetailsComponent implements OnInit {
 
   async resetDangling() {
     const danglingErredRestaurants = this.cycle.restaurants.filter(rt => rt.error === 'dangling invoices found');
+    // const danglingErredRestaurants = this.cycle.restaurants.filter(rt => rt.error === "start date is too old to auto process");
     // const newRestaurants = JSON.parse(JSON.stringify(this.cycle.restaurants));
     for (let rt of danglingErredRestaurants) {
       delete rt.error;
@@ -272,88 +273,4 @@ export class CycleDetailsComponent implements OnInit {
     return this.paymentMeansDict[row._id + (row.invoice && row.invoice.balance > 0 ? 'Send' : 'Receive')];
   }
 
-  async fix() {
-    // const idStrings = `5a865f4ea176e414008e1259
-    // 5b237c0af377d814002be013
-    // 5ae31eb54a831a14009c9a63
-    // 5b74d19b15b23a140072bc61
-    // 5cd265ee2c667c6b2eb45044
-    // 5b76baa22975d714008b4ddb
-    // 5b8f3345c1c1201400b07060
-    // 5ae1cdcc8cc7d6140063f0fb
-    // 5b3ec8d0b2689d1400659995
-    // 5a6698a9beb29a1400cbce56
-    // 5ad007188aad6414006912af
-    // 5ae3f060cff49203174f4440
-    // 5afd33f43913321400d9b5ee
-    // 5ae31c6d4a831a14009c9a61
-    // 5ba61095cf04a814008d9a36
-    // 5a5db237ecb7931400500a1c
-    // 5b5ede070c609414001f4866
-    // 5aa75157ba22271400d0ecfd
-    // 5cec7adc2c667c6b2eb451ba
-    // 5b7287e7b35ba414005a48be
-    // 5a67dfde5182611400ae8e7a
-    // 5aab6b30aad892140095f36a
-    // 5b46b56e140023140072de9b
-    // 5cb72fd32c667c6b2eb44f6c
-    // 5aa1f1735f93611400b4e4dc
-    // 5a8661eba176e414008e125e
-    // 5b8f71e0c1c1201400b07123
-    // 5c8b25a52c667c6b2eb44cf8
-    // 5b6027de9f49c91400b7bf3b
-    // 5a7570a314cdc5140050b682
-    // 5bc0024dbaaa111400ef3bcd
-    // 5ac1703f9326b814005753d2
-    // 5ab23ff7af32e21400fba329
-    // 5c8b26f92c667c6b2eb44cf9
-    // 58b8c6a92a60a61100055e37
-    // 5a1bac56e9575314005d1f49
-    // 5aa642a3ba22271400d0ec1e
-    // 5b406b81853cf914001fbffe
-    // 5ad70122cff49203174f4428
-    // 5af6dffccff49203174f4b67
-    // 5b5a9fc7c926a814000cd0fa
-    // 5aa1145c5f93611400b4e42b
-    // 5cadfe3c2c667c6b2eb44f2e
-    // 5b2276bf1b1f65140057b3e6
-    // 5c9a819d2c667c6b2eb44e6f
-    // 5b74cd3f15b23a140072bc09
-    // 5b73f5d5719c121400420f96
-    // 5ad739e1ca38201400531831
-    // 5bacbc2a24428e1400190ca4
-    // 5ae97aa588716f1400705802
-    // 5a696fe2c3908414002efc19
-    // 5ae0a7b43323291400fbb9eb
-    // 5b16073d0ab74c14002be942
-    // 5b7d37be6751321400b8a245
-    // 5b23434ff377d814002bdffa
-    // 5a6144ae3270b6140009b5a7
-    // 5a33c99f0dbc271400c0c151
-    // 5d42d07ea45ea64ae6ee7185
-    // 59f1b4a4ae34191200a945b8`;
-
-    // const ids = idStrings.split('\n').map(id => id.trim());
-
-    // // clear all processedAt and error
-    // this.cycle.restaurants.map(r => {
-    //   if(ids.indexOf(r._id) >= 0) {
-    //     delete r.error;
-    //     delete r.processedAt;
-    //   }
-    // });
-
-    // await this._api.patch(environment.qmenuApiUrl + 'generic?resource=cycle', [
-    //   {
-    //     old: {
-    //       _id: this.cycle._id
-    //     },
-    //     new: {
-    //       _id: this.cycle._id,
-    //       restaurants: this.cycle.restaurants
-    //     }
-    //   }
-    // ]).toPromise();
-
-  }
 }
