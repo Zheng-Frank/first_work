87d31115b78e95ead6898a8b13dc3dddd37748a0
rezmoth <leon.bouchan@gmail.com>
2019-10-01 15:47:06 -0500

    Custom web client with 100 seconds of timeout

---
+++ src/app/components/change-ownership/change-ownership.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChangeOwnershipComponent } from './change-ownership.component';
+
+describe('ChangeOwnershipComponent', () => {
+  let component: ChangeOwnershipComponent;
+  let fixture: ComponentFixture<ChangeOwnershipComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChangeOwnershipComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChangeOwnershipComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
