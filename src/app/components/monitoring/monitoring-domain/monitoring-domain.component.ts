45cec26665918821f8b89481e16162e66fb54fd7
unknown <mega5329@yahoo.com>
2019-10-13 22:38:03 -0400

    show all the aws/godaddy domains

--- src/app/components/monitoring/monitoring-domain/monitoring-domain.component.ts
+++ src/app/components/monitoring/monitoring-domain/monitoring-domain.component.ts
@@ -8,7 +8,37 @@ import { Helper } from 'src/app/classes/helper';
   templateUrl: './monitoring-domain.component.html',
   styleUrls: ['./monitoring-domain.component.css']
 })
-export class MonitoringDomainComponent{
+export class MonitoringDomainComponent implements OnInit {
+  domains = [];
+  filteredDomains = [];
+  managedDomain = 'Manged Domains';
+  constructor(private _api: ApiService, private _global: GlobalService) { }
+  async ngOnInit() {
+    const domainBatchSize = 1000;
+    while (true) {
+      const batch = await this._api.get(environment.qmenuApiUrl + 'generic', {
+        resource: 'domain',
+        skip: this.domains.length,
+        limit: domainBatchSize
+      }).toPromise();
+      this.domains.push(...batch);
+      if (batch.length === 0 || batch.length < domainBatchSize) {
+        break;
+      }
+    }
+    this.filter();
+  }
 
+  filter() {
+    this.filteredDomains = this.domains.slice(0);
+    switch (this.managedDomain) {
+      case 'only active':
+        const now = new Date();
+        this.filteredDomains = this.filteredDomains.filter(e => e.status === 'ACTIVE' || new Date(e.expiry).valueOf() > now.valueOf());
+        break;
+      default:
+        break;
+    }
+  }
 
 }
