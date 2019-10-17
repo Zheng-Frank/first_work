b61b3b39a9355b72e0b4ca02500c6d2c089b1d44
Leon Bouchan <leon.bouchan@gmail.com>
2019-10-17 11:02:18 -0500

    Change ownership to test

--- src/app/components/change-ownership/change-ownership.component.ts
+++ src/app/components/change-ownership/change-ownership.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from "../../../environments/environment";
+import { ApiService } from "../../services/api.service";
 
 @Component({
   selector: 'app-change-ownership',
@@ -13,7 +14,7 @@ export class ChangeOwnershipComponent implements OnInit {
   newAlias = "nino's-pasta-pizza-subs";
   switchingDate = new Date("Oct 1 2019 00:00:01 GMT-0400 (Eastern Daylight Time)");
 
-  constructor() { }
+  constructor(private _api: ApiService) { }
 
   ngOnInit() {
   }
@@ -31,117 +32,111 @@ export class ChangeOwnershipComponent implements OnInit {
   async changeOwnership() {
     const previousRestaurantId = this.oldRestaurantId;
 
-    // === Simulate the actual api callback to check correctness
-    const savedValues: any = {
-      oldRestaurantId: this.oldRestaurantId,
-      newName: this.newName,
-      previousRestaurantId,
-      newAlias: this.newAlias,
-      switchingDate: this.switchingDate
+    // const savedValues: any = {
+    //   oldRestaurantId: this.oldRestaurantId,
+    //   newName: this.newName,
+    //   previousRestaurantId,
+    //   newAlias: this.newAlias,
+    //   switchingDate: this.switchingDate
+    // }
+    // console.warn(savedValues);
+
+    const oldRestaurant = (await this._api.get(environment.qmenuApiUrl + 'generic', {
+      resource: 'restaurant',
+      query: {
+        _id: { $oid: this.oldRestaurantId }
+      },
+      limit: 1
+    }).toPromise())[0];
+
+    console.log(oldRestaurant);
+    const existingOnes = await this._api.get(environment.qmenuApiUrl + 'generic', {
+      resource: 'restaurant',
+      query: {
+        "googleAddress.place_id": oldRestaurant.googleAddress.place_id
+      },
+      limit: 2
+    }).toPromise();
+    if (existingOnes.length > 1) {
+      return alert('Failed: Already have multiple restaurants with same place ID.');
     }
-    console.warn(savedValues);
-    // =====================================================
 
-    // ================================================
-    // --- NOTE: UNCOMMENT BELOW CODE AFTER TESTING ---
-    // ================================================
+    const clone = JSON.parse(JSON.stringify(oldRestaurant));
+    delete clone._id;
+    clone.createdAt = new Date();
+    clone.updatedAt = new Date();
+    (clone.rateSchedules || []).map(rs => rs.agent = 'none');
+    delete clone.notifications;
+    delete clone.closedHours;
+    clone.name = this.newName;
+    clone.previousRestaurantId = previousRestaurantId;
+    clone.logs = clone.logs || [];
+    clone.logs.push({
+      "problem": "change ownership",
+      "response": "this is the new. Old one is " + oldRestaurant._id,
+      "time": new Date(),
+      "username": "system",
+      "resolved": true
+    });
 
-    // const oldRestaurant = (await this._api.get(environment.qmenuApiUrl + 'generic', {
-    //   resource: 'restaurant',
-    //   query: {
-    //     _id: { $oid: oldRestaurantId }
-    //   },
-    //   limit: 1
-    // }).toPromise())[0];
+    const oldPatch: any = {
+      old: { _id: oldRestaurant._id },
+      new:
+      {
+        _id: oldRestaurant._id,
+        disabled: true
+      }
+    };
 
-    // console.log(oldRestaurant);
-    // const existingOnes = await this._api.get(environment.qmenuApiUrl + 'generic', {
-    //   resource: 'restaurant',
-    //   query: {
-    //     "googleAddress.place_id": oldRestaurant.googleAddress.place_id
-    //   },
-    //   limit: 2
-    // }).toPromise();
-    // if (existingOnes.length > 1) {
-    //   return alert('Failed: Already have multiple restaurants with same place ID.');
-    // }
+    if (oldRestaurant.alias === this.newAlias) {
+      // patch old to a new alias
+      oldPatch.new.alias = oldRestaurant.alias + "-old";
+    } else {
+      // use new alias directly
+      clone.alias = this.newAlias;
+    }
 
-    // const clone = JSON.parse(JSON.stringify(oldRestaurant));
-    // delete clone._id;
-    // clone.createdAt = new Date();
-    // clone.updatedAt = new Date();
-    // (clone.rateSchedules || []).map(rs => rs.agent = 'none');
-    // delete clone.notifications;
-    // delete clone.closedHours;
-    // clone.name = newName;
-    // clone.previousRestaurantId = previousRestaurantId;
-    // clone.logs = clone.logs || [];
-    // clone.logs.push({
-    //   "problem": "change ownership",
-    //   "response": "this is the new. Old one is " + oldRestaurant._id,
-    //   "time": new Date(),
-    //   "username": "system",
-    //   "resolved": true
-    // });
+    if (oldRestaurant.name === this.newName) {
+      oldPatch.new.name = oldRestaurant.name + ' - old';
+    }
 
-    // const oldPatch: any = {
-    //   old: { _id: oldRestaurant._id },
-    //   new:
-    //   {
-    //     _id: oldRestaurant._id,
-    //     disabled: true
-    //   }
-    // };
+    const ordersToMigrate = await this._api.get(environment.qmenuApiUrl + 'generic', {
+      resource: 'order',
+      query: {
+        restaurant: { $oid: oldRestaurant._id },
+        createdAt: { $gt: { $date: this.switchingDate } }
+      },
+      projection: {
+        createdAt: 1
+      },
+      limit: 8000
+    }).toPromise();
 
-    // if (oldRestaurant.alias === newAlias) {
-    //   // patch old to a new alias
-    //   oldPatch.new.alias = oldRestaurant.alias + "-old";
-    // } else {
-    //   // use new alias directly
-    //   clone.alias = newAlias;
-    // }
+    // start the action!
+    // 1. create the new restaurant!
+    const resultIds = await this._api.post(environment.qmenuApiUrl + 'generic?resource=restaurant', [clone]).toPromise();
 
-    // if (oldRestaurant.name === newName) {
-    //   oldPatch.new.name = oldRestaurant.name + ' - old';
-    // }
+    // 2. now path those orders's restaurant field
+    await this._api.patch(environment.qmenuApiUrl + 'generic?resource=order',
+      ordersToMigrate.map(order => ({
+        old: { _id: order._id },
+        new: { _id: order._id, restaurant: { $oid: resultIds[0] } },
+      }))
+    ).toPromise();
 
-    // const ordersToMigrate = await this._api.get(environment.qmenuApiUrl + 'generic', {
-    //   resource: 'order',
-    //   query: {
-    //     restaurant: { $oid: oldRestaurant._id },
-    //     createdAt: { $gt: { $date: switchingDate } }
-    //   },
-    //   projection: {
-    //     createdAt: 1
-    //   },
-    //   limit: 8000
-    // }).toPromise();
+    oldPatch.new.logs = oldRestaurant.logs || [];
+    oldPatch.new.logs.push({
+      "problem": "change ownership",
+      "response": "new RT id is " + resultIds[0],
+      "time": new Date(),
+      "username": "system",
+      "resolved": true
+    });
 
-    // // start the action!
-    // // 1. create the new restaurant!
-    // const resultIds = await this._api.post(environment.qmenuApiUrl + 'generic?resource=restaurant', [clone]).toPromise();
+    // 3. patch old restaurant!
+    await this._api.patch(environment.qmenuApiUrl + 'generic?resource=restaurant', [oldPatch]).toPromise();
 
-    // // 2. now path those orders's restaurant field
-    // await this._api.patch(environment.qmenuApiUrl + 'generic?resource=order',
-    //   ordersToMigrate.map(order => ({
-    //     old: { _id: order._id },
-    //     new: { _id: order._id, restaurant: { $oid: resultIds[0] } },
-    //   }))
-    // ).toPromise();
-
-    // oldPatch.new.logs = oldRestaurant.logs || [];
-    // oldPatch.new.logs.push({
-    //   "problem": "change ownership",
-    //   "response": "new RT id is " + resultIds[0],
-    //   "time": new Date(),
-    //   "username": "system",
-    //   "resolved": true
-    // });
-
-    // // 3. patch old restaurant!
-    // await this._api.patch(environment.qmenuApiUrl + 'generic?resource=restaurant', [oldPatch]).toPromise();
-
-    // alert('Done! ' + resultIds[0]);
+    alert('Done! ' + resultIds[0]);
   }
 
 }
