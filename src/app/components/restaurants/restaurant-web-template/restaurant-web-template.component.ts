315817ac681a892947085b5d8ade9fec395a8e27
leon bouchan <leon.bouchan@gmail.com>
2020-02-03 17:58:45 -0600

    Correct API Endpoint

--- src/app/components/restaurants/restaurant-web-template/restaurant-web-template.component.ts
+++ src/app/components/restaurants/restaurant-web-template/restaurant-web-template.component.ts
@@ -32,7 +32,7 @@ export class RestaurantWebTemplateComponent implements OnInit {
   async createTemplate() {
     try {
       // --- Populate db with template info
-      const result = await this._api.post(environment.localAutomationNgrok + 'scan-rt-website-template', {
+      const result = await this._api.post(environment.qmenuApiUrl + 'scan-rt-website-template', {
         website: this.restaurant.web.qmenuWebsite,
         id: this.restaurant._id
       }).toPromise();
