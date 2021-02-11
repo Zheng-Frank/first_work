0f16a665d3c1d919947ddfd6a0e2a39d3e73147f
Jackson Todd <jacksonttodd@gmail.com>
2021-02-11 13:07:36 -0500

    Re-factoring component to handle multiple years for a single restaurant

--- src/app/components/restaurants/restaurant-form1099-k/restaurant-form1099-k.component.ts
+++ src/app/components/restaurants/restaurant-form1099-k/restaurant-form1099-k.component.ts
@@ -32,26 +32,24 @@ export class Form1099KComponent implements OnInit {
   orderCount = 0;
   sumOfTransactions = 0;
 
-  formLink;
+  formLinks = [];
+  allFormDataForThisRestaurant = [];
 
   @Input() restaurant: Restaurant;
-
 
   constructor(private _route: ActivatedRoute, private _api: ApiService, private _global: GlobalService, private currencyPipe: CurrencyPipe, private datePipe: DatePipe, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
-    this.populateOrders();
   }
 
-
-  async populateOrders() {
+  async populateOrders(year) {
     const query = {
       restaurant: {
         $oid: this.restaurant._id
       },
       "paymentObj.method": "QMENU",
       $expr: {
-        $eq: [{ $year: "$createdAt" }, 2020]
+        $eq: [{ $year: "$createdAt" }, year]
       }
     } as any;
 
@@ -64,10 +62,8 @@ export class Form1099KComponent implements OnInit {
       sort: {
         createdAt: -1
       }
-    }, 50);
+    }, 200);
     console.log(orders);
-    // get blocked customers and assign back to each order blacklist reasons
-
 
     // assemble back to order:
     this.orders = orders.map(order => {
@@ -81,19 +77,23 @@ export class Form1099KComponent implements OnInit {
     });
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    // const lastYear = new Date().getFullYear() - 1;
-    // console.log(this.form1099KIndexOf(2020));
-    // if (this.form1099KIndexOf(lastYear) >= 0) {
-    //   this.generateForm1099kPDF(this.restaurant.form1099ks[this.form1099KIndexOf(lastYear)]);
-    // } else if (this.restaurant) {
-    // this.populateOrders();
-    // }
+  async ngOnChanges(changes: SimpleChanges) {
+    const startingYear = 2020;
+    const endingYear = new Date().getFullYear();
+    for (let year = startingYear; year <= endingYear; year += 1) {
+      if (this.form1099KIndexOf(year) >= 0) {
+        await this.generateForm1099kPDF(this.restaurant.form1099ks[this.form1099KIndexOf(year)]);
+      } else if (this.restaurant) {
+        await this.populateOrders(year);
+        await this.checkIndividualRestaurant(year);
+      }
+    }
+    console.log(this.formLinks);
   }
 
   form1099KIndexOf(year) {
     let index = -1;
-    if (this.restaurant.form1099ks) {
+    if (Array.isArray(this.restaurant.form1099ks)) {
       this.restaurant.form1099ks.forEach((form1099kObj, i) => {
         if (form1099kObj.year === year) {
           index = i;
@@ -104,13 +104,12 @@ export class Form1099KComponent implements OnInit {
     return index;
   }
 
-  async checkIndividualRestaurant() {
+  async checkIndividualRestaurant(year) {
     let restaurantTotals = {
       orderCount: 0,
       sumOfTransactions: 0
     };
-    console.log(this.orders);
-    console.log(restaurantTotals.orderCount, restaurantTotals.sumOfTransactions)
+
     for (const order of this.orders) {
       if (order.orderStatuses[order.orderStatuses.length - 1].status !== "CANCELED") {
         const total = order.getTotal();
@@ -130,7 +129,7 @@ export class Form1099KComponent implements OnInit {
       const form1099KData = {
         name: this.restaurant.name,
         formUrl: '',
-        year: new Date().getFullYear() - 1,
+        year: year,
         orderCount: restaurantTotals.orderCount,
         sumOfTransactions: restaurantTotals.sumOfTransactions,
         streetAddress: restaurantAddress.street_number + " " + restaurantAddress.route,
@@ -158,7 +157,6 @@ export class Form1099KComponent implements OnInit {
           form1099KData.monthlyTotals[new Date(order.createdAt).getMonth()] += total;
         }
       }
-
       this.generateForm1099kPDF(form1099KData);
     } else {
       this.displayMessage = true;
@@ -166,6 +164,19 @@ export class Form1099KComponent implements OnInit {
   }
 
   async generateForm1099kPDF(form1099KData) {
+    switch (form1099KData.year) {
+      case 2021:
+        this.generateYear2021Form(form1099KData);
+        break;
+      case 2020:
+        this.generateYear2020Form(form1099KData)
+        break;
+      default:
+        console.log('Form generation is not supported for this year');
+    }
+  }
+
+  async generateYear2020Form(form1099KData) {
     const formTemplateUrl = "../../../../assets/form1099k/f1099k_2020.pdf";
     const formBytes = await fetch(formTemplateUrl).then((res) => res.arrayBuffer());
     const pdfDoc = await PDFDocument.load(formBytes);
@@ -252,8 +263,99 @@ export class Form1099KComponent implements OnInit {
 
     const link = document.createElement('a');
     link.href = window.URL.createObjectURL(blob);
-    link.download = "Form1099K.pdf";
-    this.formLink = link;
+    link.download = `f1099k_2020_${form1099KData.name}`;
+    this.formLinks.push(link);
+  }
+
+  async generateYear2021Form(form1099KData) {
+    const formTemplateUrl = "../../../../assets/form1099k/f1099k_2021.pdf";
+    const formBytes = await fetch(formTemplateUrl).then((res) => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(formBytes);
+    const form = pdfDoc.getForm();
+
+    /* This block of code will retrieve & log all the field names in a file. If you already know the field names for a given PDF, you don't need to run this. */
+
+    // const fields = form.getFields();
+    // fields.forEach(field => {
+    //   const name = field.getName();
+    //   console.log('Field name: ', name);
+    // })
+
+    const qMenuAddress = `
+    qMenu, Inc.
+    107 Technology Pkwy NW, Ste. 211
+    Peachtree Corners, GA 30092`;
+
+    // Filer's name
+    form.getTextField(`topmostSubform[0].CopyB[0].LeftCol[0].f2_1[0]`).setText(qMenuAddress);
+    // Filer checkbox
+    form.getCheckBox(`topmostSubform[0].CopyB[0].LeftCol[0].FILERCheckbox_ReadOrder[0].c2_3[0]`).check();
+
+    // Transaction reporting checkbox
+    form.getCheckBox(`topmostSubform[0].CopyB[0].LeftCol[0].c2_5[0]`).check();
+    // Payee's name
+    form.getTextField(`topmostSubform[0].CopyB[0].LeftCol[0].f2_2[0]`).setText(form1099KData.name);
+    // Street address
+    form.getTextField(`topmostSubform[0].CopyB[0].LeftCol[0].f2_3[0]`).setText(form1099KData.streetAddress);
+    // City, State, and ZIP code
+    form.getTextField(`topmostSubform[0].CopyB[0].LeftCol[0].f2_4[0]`).setText(form1099KData.cityStateAndZip);
+    // PSE name and telephone
+    form.getTextField(`topmostSubform[0].CopyB[0].LeftCol[0].f2_5[0]`).setText('');
+    // Account number
+    form.getTextField(`topmostSubform[0].CopyB[0].LeftCol[0].f2_6[0]`).setText('');
+    // Filer's TIN
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_7[0]`).setText('81-4208444');
+    // Payee's TIN
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_8[0]`).setText('');
+    // Box 1a Gross amount
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_9[0]`).setText(form1099KData.sumOfTransactions.toFixed(2))
+    // Box 1b card not present transactions
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box1b_ReadOrder[0].f2_10[0]`).setText(form1099KData.sumOfTransactions.toFixed(2))
+    // Box 2 - Merchant category code (Always 5812 for restaurants)
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_11[0]`).setText('5812');
+    // Box 3 - Number of payment transactions
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_12[0]`).setText(form1099KData.orderCount.toString())
+    // Box 4 - Federal income tax withheld
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_13[0]`).setText('');
+    // Box 5a - January income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box5a_ReadOrder[0].f2_14[0]`).setText(form1099KData.monthlyTotals[0].toFixed(2));
+    // Box 5b - February income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_15[0]`).setText(form1099KData.monthlyTotals[1].toFixed(2));
+    // Box 5c - March income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box5c_ReadOrder[0].f2_16[0]`).setText(form1099KData.monthlyTotals[2].toFixed(2))
+    // Box 5d - April income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_17[0]`).setText(form1099KData.monthlyTotals[3].toFixed(2))
+    // Box 5e - May income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box5e_ReadOrder[0].f2_18[0]`).setText(form1099KData.monthlyTotals[4].toFixed(2));
+    // Box 5f - June income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_19[0]`).setText(form1099KData.monthlyTotals[5].toFixed(2));
+    // Box 5g - July income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box5g_ReadOrder[0].f2_20[0]`).setText(form1099KData.monthlyTotals[6].toFixed(2));
+    // Box 5h - August income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_21[0]`).setText(form1099KData.monthlyTotals[7].toFixed(2));
+    // Box 5i - September income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box5i_ReadOrder[0].f2_22[0]`).setText(form1099KData.monthlyTotals[8].toFixed(2));
+    // Box 5j - October income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_23[0]`).setText(form1099KData.monthlyTotals[9].toFixed(2));
+    // Box 5k - November income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box5k_ReadOrder[0].f2_24[0]`).setText(form1099KData.monthlyTotals[10].toFixed(2));
+    // Box 5l - December income
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].f2_25[0]`).setText(form1099KData.monthlyTotals[11].toFixed(2));
+    // Box 6 - State
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box6_ReadOrder[0].f2_26[0]`).setText(form1099KData.state);
+    // Box 7 - State ID
+    form.getTextField(`topmostSubform[0].CopyB[0].RightCol[0].Box7_ReadOrder[0].f2_28[0]`).setText('');
+
+    const outputDoc = await PDFDocument.create();
+    const [fourthPage] = await outputDoc.copyPages(pdfDoc, [3])
+    outputDoc.addPage(fourthPage);
+    const pdfBytes = await outputDoc.save();
+    const blob = new Blob([pdfBytes], { type: "application/pdf" });
+
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = `f1099k_2021_${form1099KData.name}`;
+    this.formLinks.push(link);
   }
 
   sanitizeLink(url: string) {
@@ -266,7 +368,7 @@ export class Form1099KComponent implements OnInit {
 
   async updateRestaurantRecord(form1099KData) {
     let restaurant1099KData;
-    if (this.restaurant.form1099ks) {
+    if (this.restaurant.form1099ks && Array.isArray(this.restaurant.form1099ks)) {
       restaurant1099KData = this.restaurant.form1099ks.push(form1099KData);
     } else {
       restaurant1099KData = [form1099KData];
