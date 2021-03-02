483084d474b016bb5666af96251e18df3939238d
VarunVerma7 <varunqmenu@Varuns-MacBook-Pro.local>
2021-03-02 18:34:22 -0500

    Added IvrAgentAnalysisComponent

---
+++ src/app/components/ivr-agent-analysis/ivr-agent-analysis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ivr-agent-analysis',
+  templateUrl: './ivr-agent-analysis.component.html',
+  styleUrls: ['./ivr-agent-analysis.component.css']
+})
+export class IvrAgentAnalysisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
