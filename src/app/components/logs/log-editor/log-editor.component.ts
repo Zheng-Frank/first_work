cc2ce3aab77e3c6028be30a7142613520ca88136
xiaofeichai <361043562@qq,com>
2021-03-18 07:34:59 +0800

    hours-of-operation

--- src/app/components/logs/log-editor/log-editor.component.ts
+++ src/app/components/logs/log-editor/log-editor.component.ts
@@ -228,5 +228,8 @@ export class LogEditorComponent implements OnInit {
   toggleGMBCalls() {
     this.log.type === 'gmb-call' ? this.log.type = undefined : this.log.type = 'gmb-call';
   }
+  toggleHoursOfOperation(){
+    this.log.type === 'hours-of-operation' ? this.log.type = undefined : this.log.type = 'hours-of-operation';
+  }
 
 }
