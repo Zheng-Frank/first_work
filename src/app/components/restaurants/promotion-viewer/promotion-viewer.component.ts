f741eae0c0169da70875118fd2432b0915d9e951
Jackson Todd <jacksonttodd@gmail.com>
2021-03-29 10:06:35 -0400

    Updates to promotion viewer

--- src/app/components/restaurants/promotion-viewer/promotion-viewer.component.ts
+++ src/app/components/restaurants/promotion-viewer/promotion-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
 import { isType } from '@angular/core/src/type';
 import { Promotion } from '@qmenu/ui';
 
@@ -7,11 +7,10 @@ import { Promotion } from '@qmenu/ui';
   templateUrl: './promotion-viewer.component.html',
   styleUrls: ['./promotion-viewer.component.css']
 })
-export class PromotionViewerComponent implements OnInit {
+export class PromotionViewerComponent implements OnInit, OnChanges {
   today = new Date();
   @Input() hideEditButton = false;
   @Input() promotionType: string;
-  @Input() eligibility: string;
   @Input() useFreeItemList: boolean;
   @Input() promotion: Promotion;
   @Input() menus = [];
@@ -36,47 +35,16 @@ export class PromotionViewerComponent implements OnInit {
 
   itemListToString(list) {
     let listString = '';
-    if (list.length === 1) {
-      return this.promotionListEntryToString(list[0]);
-    } else {
-      list.forEach((entry, i) => {
-        if (i === list.length - 1) {
-          listString += 'or ' + this.promotionListEntryToString(entry);
-        } else {
-          listString += this.promotionListEntryToString(entry) + ', ';
-        }
-      })
-    }
     return listString;
   }
 
   freeItemListToString(list) {
     let listString = '';
-    if (list.length === 1) {
-      return list[0].mcs[0].mis[0].name.trim();
-    } else {
-      list.forEach((entry, i) => {
-        if (i === list.length - 1) {
-          listString += 'or ' + entry.mcs[0].mis[0].name.trim();
-        } else {
-          listString += entry.mcs[0].mis[0].name.trim() + ', ';
-        }
-      })
-    }
     return listString;
   }
 
 
   promotionListEntryToString(entry) {
-    if (!entry.mcs) {
-      return entry.name.trim();
-    } else {
-      if (entry.mcs[0].mis) {
-        return `${entry.name.trim()}➜${entry.mcs[0].name.trim()}➜${entry.mcs[0].mis[0].name.trim()}`;
-      } else {
-        return `${entry.name.trim()}➜${entry.mcs[0].name.trim()}`;
-      }
-    }
   }
 
   edit() {
