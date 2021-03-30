bc3593a69fad12b45126672a19a84a25737d5cbb
xiaofeichai <361043562@qq,com>
2021-03-30 10:37:46 +0800

    remote a row code

--- src/app/components/logs/log-editor/log-editor.component.ts
+++ src/app/components/logs/log-editor/log-editor.component.ts
@@ -173,7 +173,6 @@ export class LogEditorComponent implements OnInit {
       if (this.hasAdjustment) {
         this.log.resolved = false;
       }
-      this.log.type=this.hoursOfOperation
       console.log('log: this.log', this.log);
       this.success.emit({
         formEvent: event,
