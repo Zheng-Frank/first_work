9cec077ab77d08f4ca6976e4e1c5209e4a512c04
garysui <garysui@gmail.com>
2021-04-01 13:07:27 -0400

    fixed menu picker

--- src/app/components/restaurants/menu-editor/menu-editor.component.ts
+++ src/app/components/restaurants/menu-editor/menu-editor.component.ts
@@ -72,13 +72,6 @@ export class MenuEditorComponent implements OnInit {
     });
     // sort!
     this.menu.hours.sort((a, b) => a.fromTime.valueOf() - b.fromTime.valueOf());
-
-    // correct offsetToEST, hour-picker is only for your LOCAL browser. We need to translate it to restaurant's hour settings
-    hours.map(h => {
-      h.fromTime = this._timezone.transformToTargetTimeUsingCurrentOffset(h.fromTime, this.timezone);
-      h.toTime = this._timezone.transformToTargetTimeUsingCurrentOffset(h.toTime, this.timezone);
-    });
-
     this.clickedAddHour = false;
   }
 
