242f142f4cef3d3dc57bccc401e22dd89ff56c0f
xiaofeichai <361043562@qq,com>
2021-04-09 10:53:04 +0800

    create qr restaurant table.

---
+++ src/app/components/restaurants/qr-restaurant-list/qr-restaurant-list.component.ts
@@ -0,0 +1,48 @@
+import { GlobalService } from 'src/app/services/global.service';
+import { environment } from 'src/environments/environment';
+import { ApiService } from 'src/app/services/api.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-restaurant-list',
+  templateUrl: './qr-restaurant-list.component.html',
+  styleUrls: ['./qr-restaurant-list.component.css']
+})
+export class QrRestaurantListComponent implements OnInit {
+
+  qrRestaurantListRows;
+  pagination = true;
+  knownUsers = [];
+  restaurantsColumnDescriptors = [
+    {
+      label: 'Number'
+    },
+    {
+      label: "Restaurant",
+      paths: ['name'],
+      sort: (a, b) => (a || '') > (b || '') ? 1 : ((a || '') < (b || '') ? -1 : 0)
+    },
+    {
+      label: "fee Schedule"
+    }
+    
+  ];
+  constructor(private _api: ApiService, private _global: GlobalService) { }
+
+  ngOnInit() {
+    this.populateQrRestaurant();
+  }
+
+  async populateQrRestaurant(){
+      this.qrRestaurantListRows = await this._api.getBatch(environment.qmenuApiUrl + 'generic', {
+      resource: 'restaurant',
+      query: { qrSettings: {$exists:true} },
+      projection:{
+
+      },
+      sort: { updatedAt: -1 }
+    }, 100000);
+    this._global.getCachedUserList().then(users => this.knownUsers = users).catch(console.error);
+  }
+
+}
