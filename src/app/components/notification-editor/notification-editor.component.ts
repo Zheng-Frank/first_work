e648cb90d64c4b05bb2d33ce2b741396346a91b0
Jackson Todd <jacksonttodd@gmail.com>
2021-04-28 16:12:03 -0400

    Added functionality to prevent saving a template with unpermitted merge fields

--- src/app/components/notification-editor/notification-editor.component.ts
+++ src/app/components/notification-editor/notification-editor.component.ts
@@ -14,6 +14,21 @@ export class NotificationEditorComponent implements OnInit {
 
   constructor() {}
 
+  allMergeFields = [
+    "_newLine_",
+    "_customerFirstName_",
+    "_customerLastName_",
+    "_restaurantName_",
+    "_orderNumber_",
+    "_orderTimeEstimate_",
+    "_orderDetailsURL_",
+    "_cancelComments_",
+    "_adjustmentAmount_",
+    "_customerLastName",
+    "_refundTimeFrame_",
+    "_loginCode_"
+  ];
+
   allowableMergeFields = {
     "New Order Placed": ["_newLine_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderTimeEstimate_", "_orderDetailsURL_"],
     "Confirmed": ["_newLine_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderTimeEstimate_", "_orderDetailsURL_"],
@@ -24,13 +39,13 @@ export class NotificationEditorComponent implements OnInit {
     "Completed": ["_newLine_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderTimeEstimate_", "_orderDetailsURL_"],
     "Restaurant Canceled - refund": ["_newLine_", "_cancelComments_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderDetailsURL_", "_adjustmentAmount_"],
     "Restaurant Canceled - no charge": ["_newLine_", "_cancelComments_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderDetailsURL_"],
-    "New Order Received": ["_newLine_"],
-    "Delivery Complete": ["_newLine_"],
+    "New Order Received": ["_newLine_", "_customerFirstName_", "_customerLastName", "_restaurantName_", "_orderNumber_"],
+    "Delivery Complete": ["_newLine_", "_customerFirstName_", "_customerLastName", "_restaurantName_", "_orderNumber_"],
     "Customer Canceled": ["_newLine_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderDetailsURL_"],
     "Adjust Order - refund": ["_newLine_", "_adjustmentAmount_", "_refundTimeFrame_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderDetailsURL_"],
     "Adjust Order - charge": ["_newLine_", "_adjustmentAmount_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderDetailsURL_"],
-    "Change to pickup - refund": ["_newLine_", "_refundTimeFrame_"],
-    "Change to pickup - charge": ["_newLine_"],
+    "Change to pickup - refund": ["_newLine_", "_adjustmentAmount_", "_refundTimeFrame_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderDetailsURL_"],
+    "Change to pickup - charge": ["_newLine_", "_adjustmentAmount_", "_customerFirstName_", "_customerLastName_", "_restaurantName_", "_orderNumber_", "_orderDetailsURL_"],
     "Order delivery status": ["_newLine_"],
     "Sesame Success": ["_newLine_"],
     "Sesame Failure": ["_newLine_"],
@@ -38,6 +53,29 @@ export class NotificationEditorComponent implements OnInit {
   }
 
   ngOnInit() {
+    const allMergeFields = [];
+    for (const key in this.allowableMergeFields) {
+      this.allowableMergeFields[key].forEach(field => {
+        if (!allMergeFields.includes(field)) {
+          allMergeFields.push(field);
+        }
+      })
+    }
+    console.log(allMergeFields);
+  }
+
+  verifyMergeFields() {
+    const illegalFields = [];
+    const allowableFields = this.allowableMergeFields[this.notification.name];
+    this.allMergeFields.forEach(field => {
+      if (this.notification.content.includes(field) && !allowableFields.includes(field)) {
+        illegalFields.push(field);
+      }
+    });
+    if (illegalFields.length) {
+      return illegalFields.join(' ');
+    }
+    return "";
   }
 
   saveNotification() {
