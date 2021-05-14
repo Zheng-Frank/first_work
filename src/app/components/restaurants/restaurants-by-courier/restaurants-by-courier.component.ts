5ba283118bf2e7df7771c6d8e614eb470b929c88
xiaofeichai <361043562@qq,com>
2021-05-14 19:48:32 +0800

    added restaurant by courier table.

--- src/app/components/restaurants/restaurants-by-courier/restaurants-by-courier.component.ts
+++ src/app/components/restaurants/restaurants-by-courier/restaurants-by-courier.component.ts
@@ -1,11 +1,7 @@
-import { Courier } from './../../../classes/courier';
 import { Helper } from './../../../classes/helper';
 import { environment } from 'src/environments/environment';
 import { ApiService } from 'src/app/services/api.service';
 import { Component, OnInit } from '@angular/core';
-import { RestaurantWithCourier } from 'src/app/classes/restaurant-courier';
-import { RestaurantCourierService } from 'src/app/classes/restaurant-courier-service';
-import { User } from 'src/app/classes/user';
 
 @Component({
   selector: 'app-restaurants-by-courier',
@@ -14,11 +10,10 @@ import { User } from 'src/app/classes/user';
 })
 export class RestaurantsByCourierComponent implements OnInit {
 
-  couriers;
-  restaurants;
-  restaurantCourierService: RestaurantCourierService;
-  filteredRestaurants;
-  availabilityList = ["All", "signed up", "available", "not available", "unknown"];
+  couriers = [];
+  courier = 'All';
+  restaurants = [];
+  filteredRestaurants = [];
 
   restaurantsColumnDescriptors = [
     {
@@ -26,17 +21,17 @@ export class RestaurantsByCourierComponent implements OnInit {
     },
     {
       label: "Restaurant",
-      paths: ['name'],
       sort: (a, b) => (a || '') > (b || '') ? 1 : ((a || '') < (b || '') ? -1 : 0)
     },
     {
+      label: "Courier",
+    },
+    {
       label: "Time Zone",
-      paths: ['timeZone'],
       sort: (a, b) => (a || '') > (b || '') ? 1 : ((a || '') < (b || '') ? -1 : 0)
     },
     {
       label: "Score",
-      paths: ['score'],
       sort: (a, b) => (a || 0) > (b || 0) ? 1 : ((a || 0) < (b || 0) ? -1 : 0)
     }
   ];
@@ -44,76 +39,46 @@ export class RestaurantsByCourierComponent implements OnInit {
   constructor(private _api: ApiService) { }
 
   ngOnInit() {
-    this.populateCourierList();
+    this.populateRestaurantListByCourier();
   }
 
   async populateRestaurantListByCourier() {
     this.couriers = await this._api.getBatch(environment.qmenuApiUrl + "generic", {
       resource: "courier",
-      query: { },
       projection: { name: 1 },
-      limit: 1
-    },1000);
-    this.restaurants = this.getRestaurants();
-  }
- 
-
-  async getRestaurants() {
-    const restaurants = await this._api.getBatch(environment.qmenuApiUrl + 'generic', {
+    }, 1000);
+    this.restaurants = await this._api.getBatch(environment.qmenuApiUrl + 'generic', {
       resource: 'restaurant',
-      query: {disabled:{$ne:true}},
+      query: { disabled: { $ne: true },"googleAddress.formatted_address":{$exists:true} },
       projection: {
         _id: 1,
         "googleAddress.formatted_address": 1,
         name: 1,
         courier: 1,
         score: 1,
-        disabled: 1,
       },
     }, 5000);
-    return this.parseRestaurants(restaurants);
+    this.restaurants = this.parseRestaurants(this.restaurants);
+    this.filteredRestaurants = this.restaurants;
   }
-  private parseRestaurants(restaurants) {
+
+  parseRestaurants(restaurants) {
     const ret = restaurants.map(each => ({
       restaurantId: each._id,
       name: each.name,
       address: each.googleAddress.formatted_address,
-      disabled: each.disabled,
       score: each.score,
       timeZone: Helper.getTimeZone(each.googleAddress.formatted_address),
-      courier :1
-      // courier: (each.courier && each.courier.name === this.courier.name) ? "signed up" : null,
+      courier: each.courier
     }));
     return ret;
   }
-  // Button: "Update Restaurants in List"
-  async updateRestaurantList() {
-    await this.restaurantCourierService.updateRestaurantList();
-    await this.refresh();
-    return;
-  }
-  // Button: "Rescan"
-  async scanPostmates() {
-    await this.restaurantCourierService.scanCourierAvailability();
-    await this.refresh();
-    return;
-  }
 
   filter() {
-    // if (this.selectedAvailability === "All") {
-    //   this.filteredRestaurants = this.restaurantList;
-    // }
-    // else if (this.selectedAvailability === "unknown") {
-    //   this.filteredRestaurants = this.restaurantList.filter(each => !(["signed up", "available", "not available"].includes(each.availability)));
-    // }
-    // else {
-    //   this.filteredRestaurants = this.restaurantList.filter(each => each.availability === this.selectedAvailability);
-    // }
-
+    if (this.courier === 'All') {
+      this.filteredRestaurants = this.restaurants;
+    } else if (this.courier) { // it's maybe undefined.
+      this.filteredRestaurants = this.restaurants.filter(each => each.courier && each.courier.name && each.courier.name === this.courier);
+    }
   }
-
- 
-
-
-  
 }
