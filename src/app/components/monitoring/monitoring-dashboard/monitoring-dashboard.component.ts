ba39efa58691e64c5f0e0ffde0f3f819c797b085
xiaofeichai <361043562@qq.com>
2021-06-18 13:44:06 +0800

    Show all orders by type and date.

--- src/app/components/monitoring/monitoring-dashboard/monitoring-dashboard.component.ts
+++ src/app/components/monitoring/monitoring-dashboard/monitoring-dashboard.component.ts
@@ -3,6 +3,21 @@ import { GlobalService } from 'src/app/services/global.service';
 import { ApiService } from 'src/app/services/api.service';
 import { environment } from 'src/environments/environment';
 
+enum orderStatsTypes{
+  PICKUP = 'PICKUP',
+  DELIVERY = 'DELIVERY',
+  DINE_IN = 'DINE-IN',
+  QR = 'QR',
+  Postmates = 'Postmates',
+  APP = 'APP',
+  iOS = 'iOS',
+  Mac = 'Mac',
+  Android = 'Android',
+  Windows = 'Windows',
+  NULL = 'null',
+  Linux = 'Linux'
+}
+
 @Component({
   selector: 'app-monitoring-dashboard',
   templateUrl: './monitoring-dashboard.component.html',
@@ -12,7 +27,22 @@ export class MonitoringDashboardComponent implements OnInit {
 
   monitoredItem = 'Dashboard Items';
   items = [];
-
+  orderStatsItemsAllTypes = [];
+  orderStatsItemsByDate = [];
+  orderStatsTypesArr = [
+    orderStatsTypes.PICKUP,
+    orderStatsTypes.DELIVERY,
+    orderStatsTypes.DINE_IN,
+    orderStatsTypes.QR,
+    orderStatsTypes.Postmates,
+    orderStatsTypes.APP,
+    orderStatsTypes.iOS,
+    orderStatsTypes.Mac,
+    orderStatsTypes.Android,
+    orderStatsTypes.Windows,
+    orderStatsTypes.NULL,
+    orderStatsTypes.Linux
+  ];
   dashboardItems = [];
   constructor(private _api: ApiService, private _global: GlobalService) {
     this.populate();
@@ -40,5 +70,45 @@ export class MonitoringDashboardComponent implements OnInit {
       limit: 10000
     }).toPromise());
     this.dashboardItems.push(...dashboardItems);
+    this.orderStatsItemsAllTypes = this.dashboardItems.filter(item => item.title === 'Daily Order Stats')[0];
+    // show the orders of last 20 days.
+    const query = {
+      $and: [
+        {
+          createdAt: {
+            $gte: { $date: (new Date(new Date().valueOf() - (20 * 24 * 3600 * 1000))) }
+          }
+        },
+        {
+          createdAt: {
+            $lte: { $date: new Date() }
+          }
+        }
+      ]
+    } as any;
+
+    const orders = await this._api.getBatch(environment.qmenuApiUrl + "generic", {
+      resource: "order",
+      query: query,
+      projection: {
+        _id: 1,
+        type: 1,
+        createdAt: 1
+      },
+      sort: {
+        createdAt: -1
+      },
+    }, 20000);
+    orders.forEach(order=>{
+      this.orderStatsTypesArr.forEach(type=>{
+        if(order.type && order.type === type){
+          this.orderStatsItemsByDate.push({
+            type:type,
+            order:order
+          });
+        }
+      });
+    });
+   
   }
 }
