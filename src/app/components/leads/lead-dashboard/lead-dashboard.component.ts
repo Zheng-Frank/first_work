9a5e16292c47d27d9f9951718f06ede20285122d
xiaofeichai <361043562@qq.com>
2021-07-31 14:58:47 +0800

    added time filter.

--- src/app/components/leads/lead-dashboard/lead-dashboard.component.ts
+++ src/app/components/leads/lead-dashboard/lead-dashboard.component.ts
@@ -32,7 +32,17 @@ export class LeadDashboardComponent implements OnInit {
   @ViewChild("callModal") callModal: ModalComponent;
   @ViewChild("myAddressPicker") myAddressPicker: AddressPickerComponent;
   @ViewChild("removeRTModal") removeRTModal: ModalComponent;
-
+  @ViewChild("timeFiltersModal") timeFilterModal: ModalComponent;
+  
+  timeFiltersFlag1 = false; // three conditions of timer filters, and one must be checked at least.
+  timeFiltersFlag2 = false;
+  timeFiltersFlag3 = false;
+  // minsBeforeClosings is an array needed in first condition of time filters, whose elements from 1 - 60.
+  minsBeforeClosings = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,
+    31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60];
+  minsBeforeClosing = 60; // the first condition of time filters 
+  timeFiltersStartHours; // the first variable of second condition of time filters 
+  timeFiltersEndHours; // the second variable of second condition of time filters 
   removeLeadsNoLogs = false; // if the value is true,we need to filter which has no logs.
   chainDelRestaurants = []; // the field is the chain restaurants which needs to be deleted.
   searchDelRTText = ''; // try to search the name of removed chain restaurant.
@@ -432,7 +442,21 @@ export class LeadDashboardComponent implements OnInit {
         }
       );
   }
-
+  /**
+   * - Is RT open today or not?
+     - Show hours they're open on current day (and line on timeline showing current time)
+   */
+  applyTimeFilters(){
+    if(!this.timeFiltersFlag1 && !this.timeFiltersFlag2 && !this.timeFiltersFlag3){
+      return this._global.publishAlert(AlertType.Danger, 'Please check one at least!');
+    }
+    this.timeFilterModal.hide();
+  }
+  
+  openTimeFiltersModal(){
+    this.timeFilterModal.show();
+  }
+  
   isAdmin() {
     return this._global.user.roles.indexOf("ADMIN") >= 0;
   }
