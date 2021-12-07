68fce01b70ae9cb13f3213c145802b52b8f5f6ad
Jackson Todd <jacksonttodd@gmail.com>
2021-12-07 14:12:47 -0500

    changed to jobs-based query system. working first version.

--- src/app/components/monitoring/fax-health-dashboard/fax-health-dashboard.component.ts
+++ src/app/components/monitoring/fax-health-dashboard/fax-health-dashboard.component.ts
@@ -1,96 +1,128 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from "../../../services/api.service";
 import { environment } from "../../../../environments/environment";
-import { GlobalService } from "../../../services/global.service";
 @Component({
   selector: 'fax-health-dashboard',
   templateUrl: './fax-health-dashboard.component.html',
   styleUrls: ['./fax-health-dashboard.component.css']
 })
 export class FaxHealthDashboardComponent implements OnInit {
-  displaySinceDate = new Date(new Date().valueOf() - 3 * 24 * 3600000);
+  queryDate = 'Last hour';
+
+  // possible to-do: add custom date query ranges
+  queryDateMap = {
+    'Last hour': new Date().valueOf() - 3600000,
+    'Last 6 hours': new Date().valueOf() - 6 * 3600000,
+    'Last 24 hours': new Date().valueOf() - 1 * 24 * 3600000,
+    'Last 3 days': new Date().valueOf() - 3 * 24 * 3600000,
+    'Last 5 days': new Date().valueOf() - 5 * 24 * 3600000,
+  };
+
   rows = [];
   myColumnDescriptors = [
     {
-      label: "Provider Name"
+      label: "Provider Name",
+      paths: ['providerName'],
+      sort: (a, b) => (a || '') > (b || '') ? 1 : ((a || '') < (b || '') ? -1 : 0)
     },
     {
-      label: "Successfully Sent Jobs"
+      label: "Successfully Sent Jobs",
+      paths: ['successfulJobs'],
+      sort: (a, b) => (a || 0) > (b || 0) ? 1 : ((a || 0) < (b || 0) ? -1 : 0)
     },
     {
-      label: "Failed Jobs"
+      label: "Failed Jobs",
+      paths: ['failedJobs'],
+      sort: (a, b) => (a || 0) > (b || 0) ? 1 : ((a || 0) < (b || 0) ? -1 : 0)
+    },
+    {
+      label: "Failure Rate (%)",
+      paths: ['failureRate'],
+      sort: (a, b) => (a || 0) > (b || 0) ? 1 : ((a || 0) < (b || 0) ? -1 : 0)
     },
     {
       label: "Errors"
-    },
-    {
-      label: "Succeeded Once"
     }
   ];
 
-  constructor(private _api: ApiService, private _global: GlobalService) { }
+  errorColumnDescriptors = [
+    {
+      label: "Message",
+      paths: ['message'],
+      sort: (a, b) => (a || '') > (b || '') ? 1 : ((a || '') < (b || '') ? -1 : 0)
+    },
+    {
+      label: "Occurrences",
+      paths: ['number'],
+      sort: (a, b) => (a || 0) > (b || 0) ? 1 : ((a || 0) < (b || 0) ? -1 : 0)
+    },
+
+  ]
+
+  constructor(private _api: ApiService) { }
 
   ngOnInit() {
-    this.populateFaxEvents();
+    this.queryJobs();
   }
 
-  async populateFaxEvents() {
-    const threeDaysAgo = new Date().valueOf() - 3 * 24 * 3600000;
-    this.displaySinceDate = new Date(threeDaysAgo);
-    const faxStatusEvents = await this._api.get(environment.qmenuApiUrl + 'generic', {
-      resource: 'event',
+  async queryJobs() {
+    const faxJobs = await this._api.getBatch(environment.qmenuApiUrl + 'generic', {
+      resource: 'job',
       query: {
-        "name": "fax-status",
-        "createdAt": { $gt: threeDaysAgo }
+        "name": "send-order-fax",
+        "createdAt": { $gt: this.queryDateMap[this.queryDate] }
       },
       projection: {
-        "params": 1,
         "logs": 1
       },
-      limit: 5000,
       sort: {
         _id: -1
       }
-    }).toPromise();
+    }, 2500);
 
-    const telnyxSuccessEvents = {};
-    const phaxioSuccessEvents = {};
-    const telnyxFailureEvents = {};
-    const phaxioFailureEvents = {};
+    this.rows = ['twilio', 'telnyx', 'phaxio'].map(providerName => {
+      // to find success jobs, first filter by provider name, then the success/delivered message
+      const successfulJobs = faxJobs
+        .filter(job => job.logs[0].providerName === providerName)
+        .filter(job => job.logs.findIndex(log => log.status === 'delivered' || log.status === 'success') >= 0);
 
-    const telnyxErrorCodes = {};
-    const phaxioErrorCodes = {};
+      // failed jobs is simply the inverse
+      const failedJobs = faxJobs
+        .filter(job => job.logs[0].providerName === providerName)
+        .filter(job => job.logs.findIndex(log => log.status === 'delivered' || log.status === 'success') < 0);
 
-    faxStatusEvents.forEach(event => {
-      // create a table successful fax events for both telnyx and phaxio
-      if (event.params && event.params.body && event.params.body.data && event.params.body.data.payload.status === 'delivered') {
-        telnyxSuccessEvents[event.params.jobId] = event;
-      }
-
-      if (event.params && event.params.body && event.params.body.success === 'true') {
-        phaxioSuccessEvents[event.params.jobId] = event;
-      }
-
-      // if a given jobId has a success event, we know its index in the array will be *before* failure events associated with the same jobId, if any exist
-      // for this reason, with a simple key lookup we can be confident there will be no overlap between the success and failure objects
-      if (event.params && event.params.body && event.params.body.data
-        && event.params.body.data.payload.status === 'failed' && !telnyxSuccessEvents[event.params.jobId]) {
-        telnyxFailureEvents[event.params.jobId] = event;
-
-        if (telnyxErrorCodes[event.params.body.data.payload.failure_reason]) {
-          telnyxErrorCodes[event.params.body.data.payload.failure_reason] += 1;
-        } else {
-          telnyxErrorCodes[event.params.body.data.payload.failure_reason] = 1;
-        }
-      }
-
-      if (event.params && event.params.body && event.params.body.success === 'false' && !phaxioSuccessEvents[event.params.jobId]) {
-        phaxioFailureEvents[event.params.jobId] = event;
-      }
+      return {
+        providerName,
+        successfulJobs: successfulJobs.length,
+        failedJobs: failedJobs.length,
+        failureRate: (100 * failedJobs.length / (successfulJobs.length ? successfulJobs.length : 1)).toFixed(1), // ternary avoids divide by 0
+        errorCodes: this.computeErrorCodes(failedJobs)
+      };
     });
-
-    console.log(phaxioFailureEvents);
-    console.log(telnyxErrorCodes);
   }
 
+  computeErrorCodes(failedJobs) {
+    const errorRows = [];
+    const errorDict = {};
+    const ignoreLogMessages = ['success', 'delivered', 'queued', 'sending', 'media.processed', 'executed'];
+
+    failedJobs.map(job => job.logs.forEach(log => {
+      if (!ignoreLogMessages.includes(log.status)) {
+        if (errorDict[log.status]) {
+          errorDict[log.status] += 1;
+        } else {
+          errorDict[log.status] = 1;
+        }
+      }
+    }));
+
+    for (let [key, val] of Object.entries(errorDict)) {
+      errorRows.push({
+        message: key,
+        number: val
+      });
+    };
+
+    return errorRows;
+  }
 }
