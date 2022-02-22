71966df8fae108fe852704f7d3ace7693954d472
xiaofeichai <361043562@qq.com>
2022-02-22 16:17:39 +0800

    fixed filter bug

--- src/app/components/monitoring/monitoring-rts-without-agreement/monitoring-rts-without-agreement.component.ts
+++ src/app/components/monitoring/monitoring-rts-without-agreement/monitoring-rts-without-agreement.component.ts
@@ -67,6 +67,9 @@ export class MonitoringRtsWithoutAgreementComponent implements OnInit {
     this.restaurants = await this._api.get(environment.qmenuApiUrl + 'generic', {
       resource: 'restaurant',
       query: {
+        disabled: {
+          $ne: true
+        }
       },
       projection: {
         name: 1,
@@ -74,17 +77,33 @@ export class MonitoringRtsWithoutAgreementComponent implements OnInit {
         'rateSchedules.agent': 1,
         "channels.value": 1,
         "channels.type": 1,
-        createdAt: 1
-      }
-    }).toPromise();
-    this.gmbBizs = await this._api.get(environment.qmenuApiUrl + 'generic', {
-      resource: 'gmbBiz',
-      projection: {
-        "qmenuId": 1,
-        "gmbOwner": 1
+        createdAt: 1,
+        gmbOwnerHistory: {
+          $slice: -1
+        }
       },
-      limit: 10000000000000000
+      limit: 20000
     }).toPromise();
+    this.restaurants.forEach(r => {
+      r.createdAt = new Date(r.createdAt);
+    });
+    this.restaurants.sort((a, b) => a.createdAt.valueOf() - b.createdAt.valueOf());
+    // this.gmbBizs = await this._api.get(environment.qmenuApiUrl + 'generic', {
+    //   resource: 'gmbBiz',
+    //   query: {
+    //     "qmenuId": {
+    //       $exists: true
+    //     },
+    //     "gmbOwner":{
+    //       $exists: true
+    //     }
+    //   },
+    //   projection: {
+    //     "qmenuId": 1,
+    //     "gmbOwner": 1
+    //   },
+    //   limit: 10000000000000000
+    // }).toPromise();
     this.jobs = await this._api.get(environment.qmenuApiUrl + "generic", {
       resource: "job",
       aggregate: [
@@ -93,14 +112,14 @@ export class MonitoringRtsWithoutAgreementComponent implements OnInit {
             "name": {
               $eq: 'send-email'
             },
-            "job.params.subject": {
+            "params.subject": {
               $eq: 'qMenu Online Service Agreement'
             }
           }
         },
         {
           $project: {
-            'paramsTo': "$params.to",
+            "paramsTo": "$params.to",
             createdAt: 1
           }
         }
@@ -112,28 +131,31 @@ export class MonitoringRtsWithoutAgreementComponent implements OnInit {
 
   filterRTs() {
     if (!this.fromDate) {
-      this.fromDate = new Date().toISOString().split("T")[0];
+      this.fromDate = this.restaurants[0].createdAt.toISOString().split("T")[0];
     }
     // filter by createdAt
     this.agents = [];
     this.gmbOwners = [];
-    this.agreementRTs = this.restaurants.filter(rt => new Date(rt.createdAt).valueOf() >= new Date(this.fromDate + "T00:00:00.000Z").valueOf());
+    this.agreementRTs = this.restaurants.filter(rt => rt.createdAt.valueOf() >= new Date(this.fromDate + "T00:00:00.000Z").valueOf());
     this.agreementRTs.forEach(rt => {
       rt.createdAt = new Date(rt.createdAt);
-      rt.agent = ((rt.rateSchedules || [])[0] || {}).agent || 'None';
+      rt.agent = (((rt.rateSchedules || [])[0] || {}).agent || '').trim() || 'None';
       // add agent to agents filter
       if (this.agents.indexOf(rt.agent) === -1) {
         this.agents.push(rt.agent);
       }
-      let emails = rt.channels.filter(({ type }) => type === 'Email').map(({ value }) => value);
-      rt.sent = this.jobs.some(job => emails.includes(job));
+      let emails = (rt.channels || []).filter(({ type }) => type === 'Email').map(({ value }) => value.trim());
+      rt.sent = this.jobs.some(job => emails.includes(job.paramsTo.trim()));
       // rt has sent service agreement, if job has qMenu Online Service Agreement subject emails
       if (rt.sent) {
-        let rtJob = this.jobs.filter(job => emails.includes(job.paramsTo))[0];
+        let rtJob = this.jobs.filter(job => emails.includes(job.paramsTo.trim()))[0];
         // use filter email sent time as sent agreement time
         rt.agreementSentAt = new Date(rtJob.createdAt);
+        let sentTime = rt.agreementSentAt.valueOf() - rt.createdAt.valueOf();
+        rt.sentDays = Number((sentTime / (24 * 3600 * 1000)).toFixed(0));
       }
-      rt.gmbOwner = this.gmbBizs.filter(gmbBiz => gmbBiz.qmenuId = rt._id)[0] || 'None';
+      // rt.gmbOwner = this.gmbBizs.filter(gmbBiz => gmbBiz.qmenuId = rt._id).map(gmbBiz => gmbBiz.gmbOwner).toString() || 'None';
+      rt.gmbOwner = ((rt.gmbOwnerHistory || [])[0] || {}).gmbOwner || 'unknown';
       // add gmbOwner to gmbOwners filter
       if (this.gmbOwners.indexOf(rt.gmbOwner) === -1) {
         this.gmbOwners.push(rt.gmbOwner);
@@ -150,7 +172,7 @@ export class MonitoringRtsWithoutAgreementComponent implements OnInit {
     }
     // filter by gmb owner
     if (this.gmbOwner && this.gmbOwner !== 'All') {
-      this.filterAgreementRTs = this.filterAgreementRTs.filter(rt => rt.agent === this.gmbOwner);
+      this.filterAgreementRTs = this.filterAgreementRTs.filter(rt => rt.gmbOwner === this.gmbOwner);
     }
     // filter by whether sent agreement
     if (this.agreementView !== agreementViewTypes.All) {
