9698ed7ddb1d0ddab749b6784799d592185fde9b
moicen <moicen@foxmail.com>
2022-02-22 18:06:04 +0800

    update gmb website status check logic, add script to find RTs with diff gmbWebsite or owner info

--- src/app/components/system/db-scripts/db-scripts.component.ts
+++ src/app/components/system/db-scripts/db-scripts.component.ts
@@ -27,6 +27,52 @@ export class DbScriptsComponent implements OnInit {
   ngOnInit() {
   }
 
+  checkWebsiteEqual(web1, web2) {
+    [/^https?:\/\//, /www\./, /\/+$/].forEach(r => {
+      web1 = (web1 || '').replace(r, '');
+      web2 = (web2 || '').replace(r, '');
+    });
+    return web1 && web1 === web2;
+  }
+
+
+  async checkGmbWebsiteOwner() {
+    const rts = await this._api.get(environment.qmenuApiUrl + 'generic', {
+      resource: 'restaurant',
+      projection: { _id: 1, 'web.qmenuWebsite': 1, 'googleListing.cid': 1},
+      limit: 20000
+    }).toPromise();
+
+    const biz =  await this._api.getBatch(environment.qmenuApiUrl + 'generic', {
+      resource: 'gmbBiz',
+      projection: {cid: 1, gmbWebsite: 1, gmbOwner: 1},
+    }, 20000);
+
+    let dict = {}
+    biz.forEach(({cid, gmbWebsite, gmbOwner}) => {
+      if (dict[cid]) {
+        console.log('repeat...', cid, gmbWebsite, gmbOwner, dict[cid])
+      }
+      if (cid && gmbWebsite) {
+        dict[cid] = {gmbWebsite, gmbOwner}
+      }
+    })
+
+    const data = []
+    rts.forEach(rt => {
+      // @ts-ignore
+      let {gmbWebsite, gmbOwner} = dict[rt.googleListing.cid] || {}
+      let qw = rt.web && rt.web.qmenuWebsite;
+      if (gmbWebsite && qw) {
+        data.push({gw: gmbWebsite, qw, owner: gmbOwner})
+      }
+    })
+    let websiteEqualNotQmenu = data.filter(({gw, qw, owner}) => this.checkWebsiteEqual(gw, qw) && owner !== 'qmenu');
+    let websiteNotEqualInQmenu = data.filter(({gw, qw, owner}) => !this.checkWebsiteEqual(gw, qw) && owner === 'qmenu');
+    console.log('gmbWebsite === qmenuWebsite and owner !== qmenu: ', websiteEqualNotQmenu)
+    console.log('gmbWebsite !== qmenuWebsite and owner === qmenu: ', websiteNotEqualInQmenu)
+  }
+
   async migratePaymentMeansOneTime() {
     const rts = await this._api.get(environment.qmenuApiUrl + 'generic', {
       resource: 'restaurant',
@@ -4038,7 +4084,7 @@ export class DbScriptsComponent implements OnInit {
       return new Date(parseInt(timestamp, 16) * 1000);
     }
 
-    /* round - helper function to address floating point math imprecision. 
+    /* round - helper function to address floating point math imprecision.
       e.g. sometimes a total may be expressed as '2.27999999999997'. we need to put that in the format '2.28' */
     const round = function (num) {
       return Math.round((num + Number.EPSILON) * 100) / 100;
